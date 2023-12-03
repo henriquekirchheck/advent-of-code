bde6fb912d94c9b1570ba485360af492623e1d76
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-12-03 01:59:11 -0300

    Made Day 1 better

--- 2023/1/src/bin/part2.rs
+++ 2023/1/src/bin/part2.rs
@@ -1,13 +1,3 @@
-use nom::{
-    branch::alt,
-    bytes::complete::{tag, take_until},
-    character::complete::line_ending,
-    combinator::map,
-    multi::many1,
-    sequence::terminated,
-    IResult,
-};
-
 fn main() {
     let input = include_str!(concat!(env!("OUT_DIR"), "/input.txt"));
     let output = process(input);
@@ -26,87 +16,13 @@ abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen
-";
+7pqrstsixteen";
         let expected = "281";
 
         assert_eq!(process(input), expected.to_owned());
     }
 }
 
-fn number_parser(input: &str) -> IResult<&str, u8> {
-    map(
-        alt((
-            tag("one"),
-            tag("two"),
-            tag("three"),
-            tag("four"),
-            tag("five"),
-            tag("six"),
-            tag("seven"),
-            tag("eight"),
-            tag("nine"),
-            tag("1"),
-            tag("2"),
-            tag("3"),
-            tag("4"),
-            tag("5"),
-            tag("6"),
-            tag("7"),
-            tag("8"),
-            tag("9"),
-        )),
-        |res| match res {
-            "one" => 1,
-            "two" => 2,
-            "three" => 3,
-            "four" => 4,
-            "five" => 5,
-            "six" => 6,
-            "seven" => 7,
-            "eight" => 8,
-            "nine" => 9,
-            char if char
-                .chars()
-                .next()
-                .map(|c| c.is_ascii_digit())
-                .unwrap_or(false) =>
-            {
-                char.parse().unwrap()
-            }
-            _ => unreachable!("Parser should give only correct input"),
-        },
-    )(input)
-}
-
-fn main_parser(input: &str) -> IResult<&str, Vec<u8>> {
-    let (rest, input) = take_until("\n")(input)?;
-
-    Ok((
-        rest,
-        (0..input.len())
-            .flat_map(|start| {
-                let input = &input[start..];
-                number_parser(input).map(|res| res.1)
-            })
-            .collect::<Vec<u8>>(),
-    ))
-}
-
-fn mult_parser(input: &str) -> IResult<&str, Vec<Vec<u8>>> {
-    many1(terminated(main_parser, line_ending))(input)
-}
-
 fn process(input: &str) -> String {
-    dbg!(mult_parser(input))
-        .unwrap()
-        .1
-        .iter()
-        .map(|ind| {
-            let first = ind.iter().next().unwrap();
-            let last = ind.iter().last().unwrap();
-            format!("{first}{last}").parse::<usize>().unwrap()
-        })
-        .sum::<usize>()
-        .to_string()
+    day::parser2(input).sum::<usize>().to_string()
 }
---
+++ 2023/1/src/lib.rs
@@ -0,0 +1,62 @@
+use std::ops::RangeFrom;
+
+type Lookup = &'static [(&'static str, u8)];
+
+const LOOKUP: Lookup = &[
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+    ("1", 1),
+    ("2", 2),
+    ("3", 3),
+    ("4", 4),
+    ("5", 5),
+    ("6", 6),
+    ("7", 7),
+    ("8", 8),
+    ("9", 9),
+];
+
+fn number_parser(input: &str, lookup: Lookup) -> Option<&u8> {
+    lookup
+        .iter()
+        .flat_map(|(slice, num)| {
+            input
+                .get(..slice.len())
+                .and_then(|input| match input == *slice {
+                    true => Some(num),
+                    false => None,
+                })
+        })
+        .next()
+}
+
+fn line_parser(input: &str, lookup_range: RangeFrom<usize>) -> impl Iterator<Item = &u8> {
+    (0..input.len())
+        .flat_map(move |start| number_parser(&input[start..], &LOOKUP[lookup_range.clone()]))
+}
+
+fn parser_base(input: &str, named_numbers: bool) -> impl Iterator<Item = usize> + '_ {
+    input
+        .lines()
+        .map(move |line| line_parser(line, (!named_numbers as usize * 9)..))
+        .map(|mut digits| {
+            let first = digits.next().unwrap();
+            let last = digits.last().unwrap_or(first);
+            (first * 10 + last) as usize
+        })
+}
+
+pub fn parser1(input: &str) -> impl Iterator<Item = usize> + '_ {
+    parser_base(input, false)
+}
+
+pub fn parser2(input: &str) -> impl Iterator<Item = usize> + '_ {
+    parser_base(input, true)
+}
